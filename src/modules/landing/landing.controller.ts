261e3e2dd30730527d514ca47adcc84334b6501e
Bryan Lown <bryan.lown@gmail.com>
2025-12-14 04:33:20 +0000

    feat(landing): add landing page controller

---
+++ src/modules/landing/landing.controller.ts
@@ -0,0 +1,12 @@
+// src/modules/landing/landing.controller.ts
+
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function landingController(request: FastifyRequest, reply: FastifyReply) {
+  const user = request.user;
+
+  return reply.view("index.pug", {
+    pageTitle: "Visual Audio",
+  });
+
+};
