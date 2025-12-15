8693bce70e707fc1be557f81ba22596dba3f346f
Bryan Lown <bryan.lown@gmail.com>
2025-12-15 17:39:29 +0000

    build(landing): update controller to use index-legacy view

--- src/modules/landing/landing.controller.ts
+++ src/modules/landing/landing.controller.ts
@@ -5,7 +5,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 export async function landingController(request: FastifyRequest, reply: FastifyReply) {
   const user = request.user;
 
-  return reply.view("index.pug", {
+  return reply.view("index-legacy.pug", {
     pageTitle: "Visual Audio",
   });
 
