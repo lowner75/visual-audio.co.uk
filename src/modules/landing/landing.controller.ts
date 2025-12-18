c1c72fa295a425702368f9edd616f4472d74b50d
Bryan Lown <bryan.lown@gmail.com>
2025-12-18 12:13:43 +0000

    refactor(landing): update controller to point to /views/legacy templates for landing and terms pages

--- src/modules/landing/landing.controller.ts
+++ src/modules/landing/landing.controller.ts
@@ -5,7 +5,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 export async function landingController(request: FastifyRequest, reply: FastifyReply) {
   const user = request.user;
 
-  return reply.view("legacy-index.pug", {
+  return reply.view("legacy/legacy-index.pug", {
     pageTitle: "Visual Audio",
   });
 
@@ -14,7 +14,7 @@ export async function landingController(request: FastifyRequest, reply: FastifyR
 export async function termsAndConditionsController(request: FastifyRequest, reply: FastifyReply) {
   const user = request.user;
 
-  return reply.view("legacy-terms.pug", {
+  return reply.view("legacy/legacy-terms.pug", {
     pageTitle: "Visual Audio",
   });
 
