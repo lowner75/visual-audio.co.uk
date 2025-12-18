7500e38f02a9804baef9ce3807acd3ae6ff4a3f4
Bryan Lown <bryan.lown@gmail.com>
2025-12-18 07:50:47 +0000

    feat(legacy): add page-specific JS for Terms & Conditions

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@
 "use strict";
 
 import luxy from "luxy.js";
-import "./modules/legacy/main";
 import initMessagesModule from "./modules/messages/messages";
+import "./modules/legacy/main";
 
 document.addEventListener("DOMContentLoaded", () => {
-
+  
   // Initialize Luxy.js for smooth scrolling and parallax effects on desktop viewports
   const luxyEl = document.getElementById("luxy");
 
@@ -20,6 +20,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  // Terms and conditions page specific JS
+  const termsPageEl = document.getElementById("terms-page");
+  if (!termsPageEl) {
+    return;
+  } else {
+
+    // Set terms and conditions nav links and active status
+    // Desktop nav links
+    document.querySelectorAll(".va-nav__link").forEach(el => {
+      if (el.id === "menu-link-terms") return;
+      el.setAttribute('href', '/');
+      el.classList.remove("is-active");
+    });
+    // Mobile nav links
+    document.querySelectorAll(".mmenu-link a").forEach(el => {
+      if (el.id === "mmenu-link-terms") return;
+      el.setAttribute('href', '/');
+      el.classList.remove("is-active");
+    });
+    document.querySelector("#nav-link-terms")?.classList.add("is-active");
+    gsap.to('.nav-background', { duration: 1, autoAlpha: 1, delay: 2 } );
+
+  }
+
+  // Initialize messages module
   initMessagesModule();
 
 });
