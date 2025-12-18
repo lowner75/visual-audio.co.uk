4c9ff453eff834ef78dd6c58554294ec9597bb4e
Bryan Lown <bryan.lown@gmail.com>
2025-12-18 07:53:43 +0000

    feat(landing): add route for Terms & Conditions page

--- src/modules/landing/landing.routes.ts
+++ src/modules/landing/landing.routes.ts
@@ -1,13 +1,18 @@
 // src/modules/landing/landing.routes.ts
 
 import { FastifyInstance } from "fastify";
-import { landingController } from "./landing.controller";
+import { landingController, termsAndConditionsController } from "./landing.controller";
 
 export async function landingRoutes(fastify: FastifyInstance) {
 
   // landing page route ...
   fastify.get("/", async (request, reply) => {
     return landingController(request, reply);
+  });
+
+  // landing page terms route ...
+  fastify.get("/terms-and-conditions", async (request, reply) => {
+    return termsAndConditionsController(request, reply);
   });
 
 }
