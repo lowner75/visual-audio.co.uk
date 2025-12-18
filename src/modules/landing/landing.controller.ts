e3e765a84b299d2bc832ace65de3b50b59ab2fe7
Bryan Lown <bryan.lown@gmail.com>
2025-12-18 07:53:59 +0000

    feat(landing): add controller logic for Terms & Conditions page

--- src/modules/landing/landing.controller.ts
+++ src/modules/landing/landing.controller.ts
@@ -5,7 +5,16 @@ import { FastifyRequest, FastifyReply } from "fastify";
 export async function landingController(request: FastifyRequest, reply: FastifyReply) {
   const user = request.user;
 
-  return reply.view("index-legacy.pug", {
+  return reply.view("legacy-index.pug", {
+    pageTitle: "Visual Audio",
+  });
+
+};
+
+export async function termsAndConditionsController(request: FastifyRequest, reply: FastifyReply) {
+  const user = request.user;
+
+  return reply.view("legacy-terms.pug", {
     pageTitle: "Visual Audio",
   });
 
