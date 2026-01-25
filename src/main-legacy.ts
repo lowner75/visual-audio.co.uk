59f3aafa7f5e5ec5ed67fc6bbd2723ddb66d4f16
Bryan Lown <bryan.lown@gmail.com>
2026-01-25 15:33:03 +0000

    chore(main): retain legacy entry for backwards compatibility

---
+++ src/main-legacy.ts
@@ -0,0 +1,16 @@
+// src/main-legacy.ts
+
+"use strict";
+
+// Import legacy module for backward compatibility
+import "./modules/legacy/main";
+
+// Import and initialize messages module
+import initMessagesModule from "./modules/messages/messages";
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // Initialize messages module
+  initMessagesModule();
+
+});
