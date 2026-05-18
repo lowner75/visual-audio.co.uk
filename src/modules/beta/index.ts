e1ebaebf895482766082bb9d83966ebec671edaf
Bryan Lown <bryan.lown@gmail.com>
2026-05-19 00:13:33 +0100

    feat(beta): update beta module registration

--- src/modules/beta/index.ts
+++ src/modules/beta/index.ts
@@ -3,13 +3,13 @@
 import { FastifyPluginAsync } from "fastify";
 import { betaAuthHook } from "./hooks/betaAuthHook";
 import { authRoutes } from "./routes/auth.routes";
-import { landingRoutes } from "./routes/landing.routes";
+import { staticRoutes } from "./routes/static.routes";
 
 export const betaModule: FastifyPluginAsync = async (fastify) => {
 
   await fastify.register(async (f) => {
     f.addHook("preHandler", betaAuthHook);
-    await f.register(landingRoutes);
+    await f.register(staticRoutes);
   });
 
   fastify.register(authRoutes);
